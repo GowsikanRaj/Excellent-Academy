de196168de38a904beb81ca7d060ae36da006797
Gowsikan P <Gowsikan P@DESKTOP-69BTRRO>
2020-01-06 13:53:19 -0500

    Creating a form for excellent academy

---
+++ ExcellentAcademy/Form.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExcellentAcademy
+{
+    public class Form
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string phone { get; set; }
+        public string cell { get; set; }
+        public string emergency { get; set; }
+        public string email { get; set; }
+        public DateTime startingdate { get; set; }
+        
+        public DateTime date { get; set; }
+        public string signature { get; set; }
+        public int fees { get; set; }
+
+        public DateTime paymentday { get; set; }
+
+        public List<student> student { get; set; }
+
+    }
+
+    public class student
+    {
+        public string Studentname { get; set; }
+        public string Grade { get; set; }
+        public string Subject { get; set; }
+        public string Days { get; set; }
+    }
+}
